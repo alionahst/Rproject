5b0791c782fefb50142a1f4702e82022e44a18fe
alionahst <aliona.96h@gmail.com>
2020-11-10 12:43:45 +0100

    R project

---
+++ R-project.R
@@ -0,0 +1,7 @@
+install.packages("readxl")
+
+data1 <- readxl::read_excel("/Users/lannysadikin/Documents/M1\ UPV/Data\ Aquisition/Rproject/data.xlsx", 1)
+data2 <- readxl::read_excel("/Users/lannysadikin/Documents/M1\ UPV/Data\ Aquisition/Rproject/data.xlsx", 2)
+data3 <- readxl::read_excel("/Users/lannysadikin/Documents/M1\ UPV/Data\ Aquisition/Rproject/data.xlsx", 3)
+data4 <- readxl::read_excel("/Users/lannysadikin/Documents/M1\ UPV/Data\ Aquisition/Rproject/data.xlsx", 4)
+
