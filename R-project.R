3e0013f113d997ecabc4b275517815ae7b7024f6
alionahst <aliona.96h@gmail.com>
2020-11-24 13:15:54 +0100

    24/11/20

--- R-project.R
+++ R-project.R
@@ -28,7 +28,7 @@ for (i in excel.sheet){
 # organise column names and fill the first 2 columns
 library(tidyr)
 for(i in names(X)){
-  names(X[[i]])[1:3] <- c("Sr_No","Accession", "Treatment")
+  names(X[[i]])[1:3] <- c("Sr_No","Code", "Treatment")
   X[[i]] <- X[[i]][-1,]
   X[[i]] <- fill(X[[i]], 1:2)
 }
@@ -39,3 +39,13 @@ Chlor_cont <- X$`Chlorophyll content`
 Gas_par <- X$`Gas Exchange parameters`
 
 
+
+
+word <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9749/1/Table_S1.docx"
+w <- paste0(d, "/location.docx")
+download.file(word, w, mode="wb")
+
+#install.packages("docxtractr")
+library(docxtractr)
+accession_loc <- docx_extract_all(read_docx(w, track_changes = NULL), guess_header = TRUE, preserve = FALSE, trim = TRUE)
+accession_loc<- as.data.frame(accession_loc)
