ae58706211444a2d4dcfe6da808479e97c55359a
alionahst <aliona.96h@gmail.com>
2020-12-11 17:06:32 +0100

    11/12/20 17:06

--- R/salt_tolerance_evaluation.R
+++ R/salt_tolerance_evaluation.R
@@ -1,20 +1,39 @@
-
-Treatment_table <- table[table$Treatment=="Treatment",]
-
 #salt tolerance index 
-STI <- numeric()
 
+# for the loop down
+STI <- numeric()
 new <- c(1:6, 8:15, 17:25)
 
 
+
+# THIS WORKS !! but we need to do 5 times
+a1<- subset(table$Shoot_Length, table$Treatment=="Control")
+a2<- subset(table$Shoot_Length, table$Treatment=="Treatment")
+tab1 <- data.frame(a1,a2)
+tab1 <- tab1 %>%
+  mutate(STI_Shoot_Length = a1/ a2)  %>%
+  select (STI_Shoot_Length)
+
+#### try to make loop
 for (i in names(table[4:8])){
   for (j in new){
+    a1<- subset(table$[i], table$Treatment=="Control")
+    a2<- subset(table$[i], table$Treatment=="Treatment")
+  }
+  #store in the column 
+}
+
+
+# other loop that is weird
+for (i in names(table[4:8])){
+# create the column
+    for (j in new){
     x <- subset(table[[i]], table$Number == j)
   STI <- c(STI, x[1]/x[2])
   }
+  #store in the column 
 }
 STI
-
 salt_tolerance_evaluation <- data.frame(STI)
 
 
