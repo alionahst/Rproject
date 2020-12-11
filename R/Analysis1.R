82741661e8a200db022620e78d0a8a38fe618eae
alionahst <aliona.96h@gmail.com>
2020-12-11 14:43:31 +0100

    11/12/20 14:43

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -4,12 +4,21 @@
 a1 <- aov(Shoot_Length ~ Treatment, data = table)
 shapiro.test(a1$residuals)
 
-a1 <- aov(Shoot_Length ~ Treatment, data = table)
+a1 <- aov(Root_length ~ Treatment, data = table)
 shapiro.test(a1$residuals)
 
+a1 <- aov(Root_length ~ Treatment, data = table)
 
-#want to make a table to 
+a1 <- aov(Root_length ~ Treatment, data = table)
+
+a1 <- aov(Root_length ~ Treatment, data = table)
+
+a1 <- aov(Root_length ~ Treatment, data = table)
+
+
+#want to make a loop
 Normality <- data.frame()
+a1 <- numeric()
 for (i in names(table)[-(1:3)]){
   a1 <- aov(i ~ Treatment, data = table)
   x <- shapiro.test(a1$residuals) 
