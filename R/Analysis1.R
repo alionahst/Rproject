2077c6c4c5c8cc6e80f0fd8d8ea956ff63a394ce
alionahst <aliona.96h@gmail.com>
2020-12-11 16:42:53 +0100

    11/12/20 16:42

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -1,21 +1,3 @@
-
-
-
-a1 <- aov(Shoot_Length ~ Treatment, data = table)
-shapiro.test(a1$residuals)
-
-a1 <- aov(Root_length ~ Treatment, data = table)
-shapiro.test(a1$residuals)
-
-a1 <- aov(Root_length ~ Treatment, data = table)
-
-a1 <- aov(Root_length ~ Treatment, data = table)
-
-a1 <- aov(Root_length ~ Treatment, data = table)
-
-a1 <- aov(Root_length ~ Treatment, data = table)
-
-
 #want to make a loop
 Normality <- data.frame()
 a1 <- numeric()
@@ -24,7 +6,65 @@ for (i in names(table)[-(1:3)]){
   x <- shapiro.test(a1$residuals) 
   Normality <- c(Normality, x)
 }
-  
+
+
+boxplot(Shoot_Length ~ Treatment, data = table)
+
+a1 <- aov(Shoot_Length ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+#Normality
+anova(a1)
+# statistical difference
+
+a1 <- aov(Root_length ~ Treatment, data = table)
+shapiro.test(a2$residuals)
+#Normality
+anova(a1)
+# statistical difference
+
+a1 <- aov(Plant_Height ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+#Normality
+anova(a1)
+# statistical difference
+
+
+a1 <- aov(Number_Leaves ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+# no normality 
+kruskal.test(Number_Leaves ~ Treatment, data = table)
+# statistical difference
+
+
+a1 <- aov(Leaf_Area ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+# no normality 
+kruskal.test(Leaf_Area ~ Treatment, data = table)
+# statistical difference
+
+
+a1 <- aov(Root_length ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+# no normality 
+kruskal.test(Root_Length ~ Treatment, data = table)
+#statistical difference
+
+
+a1 <- aov(Fresh_Weight ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+# no normality 
+kruskal.test(Fresh_Weight ~ Treatment, data = table)
+
+
+a1 <- aov(Dry_Weight ~ Treatment, data = table)
+shapiro.test(a1$residuals)
+# no normality 
+kruskal.test(Dry_Weight ~ Treatment, data = table)
+
+
+
+
+
   
   
   
---
+++ R/salt_tolerance_evaluation.R
@@ -0,0 +1,9 @@
+
+Treatment_table <- table[table$Treatment=="Treatment",]
+
+#salt tolerance evaluation 
+STI <- c()
+
+for (i in names(table)[4:8]){}
+
+MFV <- c()
