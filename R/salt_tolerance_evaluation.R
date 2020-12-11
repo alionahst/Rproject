405b2ff71d043e98cdd89bb13e7272206475c542
alionahst <aliona.96h@gmail.com>
2020-12-11 16:43:26 +0100

    11/12/20 16:43

--- R/salt_tolerance_evaluation.R
+++ R/salt_tolerance_evaluation.R
@@ -1,9 +1,27 @@
 
 Treatment_table <- table[table$Treatment=="Treatment",]
 
-#salt tolerance evaluation 
-STI <- c()
+#salt tolerance index 
+STI <- numeric()
 
-for (i in names(table)[4:8]){}
+new <- c(1:6, 8:15, 17:25)
+
+
+for (i in names(table[4:8])){
+  for (j in new){
+    x <- subset(table[[i]], table$Number == j)
+  STI <- c(STI, x[1]/x[2])
+  }
+}
+STI
+
+salt_tolerance_evaluation <- data.frame(STI)
+
+
+
+
+
 
 MFV <- c()
+
+
