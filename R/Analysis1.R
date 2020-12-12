c0d4d78d1fd662aa19a5f3a0de504217e00b6ded
alionahst <aliona.96h@gmail.com>
2020-12-12 14:25:38 +0100

    12/12/20 14:25

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -2,69 +2,76 @@
 Normality <- data.frame()
 a1 <- numeric()
 for (i in names(table)[-(1:3)]){
-  a1 <- aov(i ~ Treatment, data = table)
+  a1 <- aov(i ~ Treat_Contr, data = table)
   x <- shapiro.test(a1$residuals) 
   Normality <- c(Normality, x)
 }
 
 
-boxplot(Shoot_Length ~ Treatment, data = table)
+boxplot(Shoot_Length ~ Treat_Contr, data = Morpho_t)
+boxplot(Root_Length ~ Treat_Contr, data = Morpho_t)
+boxplot(Height_Height ~ Treat_Contr, data = Morpho_t)
+boxplot(Number_Leaves ~ Treat_Contr, data = Morpho_t)
+boxplot(Leaf_Area ~ Treat_Contr, data = Morpho_t)
 
-a1 <- aov(Shoot_Length ~ Treatment, data = table)
+
+a1 <- aov(Shoot_Length ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
 #Normality
-anova(a1)
+kruskal.test(Shoot_Length ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
-a1 <- aov(Root_length ~ Treatment, data = table)
+a2 <- aov(Root_Length ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a2$residuals)
 #Normality
 anova(a1)
 # statistical difference
 
-a1 <- aov(Plant_Height ~ Treatment, data = table)
+
+a1 <- aov(Plant_Height ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
-#Normality
+# No normality
 anova(a1)
 # statistical difference
 
 
-a1 <- aov(Number_Leaves ~ Treatment, data = table)
+a1 <- aov(Number_Leaves ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
-# no normality 
-kruskal.test(Number_Leaves ~ Treatment, data = table)
+# No normality 
+kruskal.test(Number_Leaves ~ Treat_Contr, data = table)
 # statistical difference
 
 
-a1 <- aov(Leaf_Area ~ Treatment, data = table)
+a1 <- aov(Leaf_Area ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
 # no normality 
-kruskal.test(Leaf_Area ~ Treatment, data = table)
+kruskal.test(Leaf_Area ~ Treat_Contr, data = table)
 # statistical difference
 
 
-a1 <- aov(Root_length ~ Treatment, data = table)
+a1 <- aov(Root_length ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
 # no normality 
-kruskal.test(Root_Length ~ Treatment, data = table)
+kruskal.test(Root_Length ~ Treat_Contr, data = table)
 #statistical difference
 
 
-a1 <- aov(Fresh_Weight ~ Treatment, data = table)
+a1 <- aov(Fresh_Weight ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
 # no normality 
-kruskal.test(Fresh_Weight ~ Treatment, data = table)
+kruskal.test(Fresh_Weight ~ Treat_Contr, data = table)
 
 
-a1 <- aov(Dry_Weight ~ Treatment, data = table)
+a1 <- aov(Dry_Weight ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
 # no normality 
-kruskal.test(Dry_Weight ~ Treatment, data = table)
+kruskal.test(Dry_Weight ~ Treat_Contr, data = table)
 
 
-
-
-
+library(agricolae)
+res <- duncan.test(a1, "Treat_Contr", DFerror, MSerror, alpha = 0.05, group=TRUE, main = NULL,console=FALSE)
+res
+plot(res)
   
   
   
