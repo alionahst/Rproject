f13f218b03e47f81b70e3b67608be348fbbea388
alionahst <aliona.96h@gmail.com>
2020-12-12 18:58:54 +0100

    12/12/20 18:58

--- R/salt_tolerance_evaluation.R
+++ R/salt_tolerance_evaluation.R
@@ -5,15 +5,12 @@ y <- data.frame(dplyr::filter(table, table$Treat_Contr=="Control"))
 y <- y[-(1:3)]
 
 
-## MORPHOLOGICAL TRAIT
-
 #salt tolerance index for morphological traits
 STI_Shoot_Length <- x[1]/y[1]
 STI_Root_Length <- x[2]/y[2]
 STI_Plant_Height <- x[3]/y[3]
 STI_Number_Leaves <- x[4]/y[4]
 STI_Leaf_Area <- x[5]/y[5]
-STI <- data.frame(STI_Shoot_Length,STI_Root_Length, STI_Plant_Height, STI_Number_Leaves, STI_Leaf_Area)
 
 #trying to make a for loop
 #STI1 <- data.frame()
@@ -24,19 +21,77 @@ STI <- data.frame(STI_Shoot_Length,STI_Root_Length, STI_Plant_Height, STI_Number
 #    name <- x[j]/x[j]
 #    vecteur <- c(vecteur, name)}}
 
-
-#Membership function value for morphological trait 
-MFV <- SL<- RL <- PH <- NL<- LA<- c()
-MFV <- STI %>%
-  mutate(SL= (STI$Shoot_Length - min(STI$Shoot_Length))/(max(STI$Shoot_Length)- min(STI$Shoot_Length)),
-         RL=(STI$Root_Length - min(STI$Root_Length))/(max(STI$Root_Length)- min(STI$Root_Length)),
-         PH=(STI$Plant_Height - min(STI$Plant_Height))/(max(STI$Plant_Height)- min(STI$Plant_Height)),
-         NL=(STI$Number_Leaves - min(STI$Number_Leaves))/(max(STI$Number_Leaves)- min(STI$Number_Leaves)),
-         LA=(STI$Leaf_Area - min(STI$Leaf_Area))/(max(STI$Leaf_Area)- min(STI$Leaf_Area)))%>%
-  select(SL, RL, PH,NL, LA)
-
-
-## BIOMASS
+#salt tolerance index for biomass
 STI_Fresh_Weight <- x[6]/y[6]
 STI_Dry_Weight <- x[7]/y[7]
+
+#salt tolerance index for RWC, EL and Chlorophyll content
+STI_Relative_water_content  <- x[8]/y[8]
+names(STI_Relative_water_content)[1] <- "Relative_water_content" # change the name of the column of the data
+STI_Electrolyte_Leakage  <- x[14]/y[14]
+STI_Chlorophyll_Content <- x[15]/y[15]
+
+# salt tolerance index for Mineral ion content
+STI_Na <- x[9]/y[9]
+STI_K <- x[10]/y[10]
+STI_Ca <- x[11]/y[11]
+STI_Mg <- x[12]/y[12]
+STI_K_Na <- x[13]/y[13]
+
+# salt tolerance index for Gas exchange attributes
+STI_Photosynthesis_rate <- x[16]/y[16]
+STI_Intercellular_CO2 <- x[17]/y[17]
+STI_Transpiration_Rate <- x[18]/y[18]
+STI_Stomata_Conductance <- x[19]/y[19]
+
+STI <- data.frame(STI_Shoot_Length,
+                  STI_Root_Length, 
+                  STI_Plant_Height, 
+                  STI_Number_Leaves, 
+                  STI_Leaf_Area, 
+                  STI_Fresh_Weight, 
+                  STI_Dry_Weight,
+                  STI_Relative_water_content, 
+                  STI_Electrolyte_Leakage,
+                  STI_Na,
+                  STI_K,
+                  STI_Ca,
+                  STI_Mg,
+                  STI_K_Na,
+                  STI_Chlorophyll_Content,
+                  STI_Photosynthesis_rate,
+                  STI_Intercellular_CO2,
+                  STI_Transpiration_Rate,
+                  STI_Stomata_Conductance)
+
+summary(STI)
+
+#Membership function value for morphological trait, biomass
+MFV <- SL<- RL <- PH <- NL<- LA<- FW <- DW <- RWC <- EL <- CC <- Na <- K <- Ca <- Mg <- K_Na <- PR <- ICO2 <- TR <- SC <- c()
+MFV <- STI %>%
+  mutate(SL = (STI$Shoot_Length - min(STI$Shoot_Length))/(max(STI$Shoot_Length)- min(STI$Shoot_Length)),
+         RL = (STI$Root_Length - min(STI$Root_Length))/(max(STI$Root_Length)- min(STI$Root_Length)),
+         PH = (STI$Plant_Height - min(STI$Plant_Height))/(max(STI$Plant_Height)- min(STI$Plant_Height)),
+         NL = (STI$Number_Leaves - min(STI$Number_Leaves))/(max(STI$Number_Leaves)- min(STI$Number_Leaves)),
+         LA = (STI$Leaf_Area - min(STI$Leaf_Area))/(max(STI$Leaf_Area)- min(STI$Leaf_Area)),
+         FW = (STI$Fresh_Weight - min(STI$Fresh_Weight))/(max(STI$Fresh_Weight)- min(STI$Fresh_Weight)) ,
+         DW = (STI$Dry_Weight - min(STI$Dry_Weight))/(max(STI$Dry_Weight)- min(STI$Dry_Weight)),
+         RWC = (STI$Relative_water_content - min(STI$Relative_water_content))/(max(STI$Relative_water_content)- min(STI$Relative_water_content)),
+         EL = (STI$Electrolyte_Leakage - min(STI$Electrolyte_Leakage))/(max(STI$Electrolyte_Leakage)- min(STI$Electrolyte_Leakage)),
+         CC = (STI$Chlorophyll_Content - min(STI$Chlorophyll_Content))/(max(STI$Chlorophyll_Content)- min(STI$Chlorophyll_Content)),
+         Na = (STI$Na - min(STI$Na))/(max(STI$Na)- min(STI$Na)),
+         K = (STI$K - min(STI$K))/(max(STI$K)- min(STI$K)),
+         Ca =(STI$Ca - min(STI$Ca))/(max(STI$Ca)- min(STI$Ca)),
+         Mg=(STI$Mg - min(STI$Mg))/(max(STI$Mg)- min(STI$Mg)),
+         K_Na= (STI$K_Na - min(STI$K_Na))/(max(STI$K_Na)- min(STI$K_Na)))%>%
+#         PR= (STI$Photsynthesis_Rate - min(STI$Photosynthesis_Rate, skipna = FALSE))/(max(STI$Photosynthesis_Rate)- min(STI$Photosynthesis_Rate)))%>%
+#         ICO2= (STI$Intercellular_CO2 - min(STI$Intercellular_CO2))/(max(STI$Intercellular_CO2)- min(STI$Intercellular_CO2)))%>%
+#         TR= (STI$Transpiration_Rate - min(STI$Transpiration_Rate))/(max(STI$Transpiration_Rate)- min(STI$Transpiration_Rate)),
+#         SC= (STI$Stomata_Conductance - min(STI$Stomata_Conductance))/(max(STI$Stomata_Conductance)- min(STI$Stomata_Conductance))
+  select(SL, RL, PH, NL, LA, FW, DW, RWC, EL, CC, Na, K, Ca, Mg, K_Na)
+
+
+
+STI <- na.omit(STI)
+
 
