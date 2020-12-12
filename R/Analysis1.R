6e7f6792ca944839755646e11be6209f9145cbd1
alionahst <aliona.96h@gmail.com>
2020-12-12 14:39:13 +0100

    12/12/20 14:39

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -17,43 +17,39 @@ boxplot(Leaf_Area ~ Treat_Contr, data = Morpho_t)
 
 a1 <- aov(Shoot_Length ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a1$residuals)
-#Normality
+#No normality
 kruskal.test(Shoot_Length ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
 a2 <- aov(Root_Length ~ Treat_Contr, data = Morpho_t)
 shapiro.test(a2$residuals)
-#Normality
-anova(a1)
+#No normality
+kruskal.test(Root_Length ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
 
-a1 <- aov(Plant_Height ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a1$residuals)
+a3 <- aov(Plant_Height ~ Treat_Contr, data = Morpho_t)
+shapiro.test(a3$residuals)
 # No normality
-anova(a1)
+kruskal.test(Plant_Height ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
 
-a1 <- aov(Number_Leaves ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a1$residuals)
+a4 <- aov(Number_Leaves ~ Treat_Contr, data = Morpho_t)
+shapiro.test(a4$residuals)
 # No normality 
-kruskal.test(Number_Leaves ~ Treat_Contr, data = table)
+kruskal.test(Number_Leaves ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
 
-a1 <- aov(Leaf_Area ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a1$residuals)
+a5 <- aov(Leaf_Area ~ Treat_Contr, data = Morpho_t)
+shapiro.test(a5$residuals)
 # no normality 
-kruskal.test(Leaf_Area ~ Treat_Contr, data = table)
+kruskal.test(Leaf_Area ~ Treat_Contr, data = Morpho_t)
 # statistical difference
 
 
-a1 <- aov(Root_length ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a1$residuals)
-# no normality 
-kruskal.test(Root_Length ~ Treat_Contr, data = table)
-#statistical difference
+########### 
 
 
 a1 <- aov(Fresh_Weight ~ Treat_Contr, data = Morpho_t)
@@ -68,6 +64,9 @@ shapiro.test(a1$residuals)
 kruskal.test(Dry_Weight ~ Treat_Contr, data = table)
 
 
+
+
+###########
 library(agricolae)
 res <- duncan.test(a1, "Treat_Contr", DFerror, MSerror, alpha = 0.05, group=TRUE, main = NULL,console=FALSE)
 res
