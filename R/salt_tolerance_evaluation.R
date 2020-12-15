28825d71cadfe939d4ebdb61058fdcebf55aadb8
alionahst <aliona.96h@gmail.com>
2020-12-15 16:10:18 +0100

    15/12/20 16:10

--- R/salt_tolerance_evaluation.R
+++ R/salt_tolerance_evaluation.R
@@ -99,8 +99,12 @@ MFV <- MFV %>%
 # dendograme 
 
 
+
+
+
+
 # Pearson test 
-# do it on raw data and not on MFV and try to make a matrix 
+#peason test with MFV table 
 cor.test(MFV$SL,MFV$RL, method="pearson") #
 cor.test(MFV$SL,MFV$PH, method="pearson") #
 cor.test(MFV$SL,MFV$NL, method="pearson") #
@@ -173,3 +177,45 @@ cor.test(MFV$CC,MFV$K_Na, method="pearson") #
 cor.test(MFV$CC,MFV$PR, method="pearson") #
 
 cor.test(MFV$K_Na,MFV$PR, method="pearson") #
+
+#install.packages('Hmisc')
+#install.packages('ggpubr')
+#install.packages('tidyverse')
+#install.packages('corrplot')
+library(dplyr)
+library(lattice)
+library(survival)
+library(Formula)
+library(ggplot2)
+library(Hmisc)
+library(ggpubr)
+library(tidyverse)
+library(Hmisc)
+library(corrplot)
+
+
+
+
+# Pearson correlation test on data table  
+# variables used in the paper 
+my_data <- select(table, Shoot_Length, Root_Length, Plant_Height, Number_Leaves, Leaf_Area, Fresh_Weight, Dry_Weight, Chlorophyll_Content, Electrolyte_Leakage, K_Na, Photsynthesis_Rate)
+my_data <- na.omit(my_data)
+# all the variables
+all_data <- na.omit(table[4:23])
+
+#get p-value of my_data
+cor_1 <- rcorr(as.matrix(my_data))
+cor_1_P <- as.data.frame(cor_1$P) # put p values into a data frame 
+# get p-value of all_data
+cor_2 <- rcorr(as.matrix(all_data))
+cor_2_P <- as.data.frame(cor_2$P) # put p values into a data frame 
+
+
+res1 <- cor.mtest(all_data, conf.level = .95)
+M1 <-cor(all_data)
+corrplot(M1, p.mat = res1$p, sig.level = .05, type = "lower")
+
+res2 <- cor.mtest(my_data, conf.level = .95)
+M2 <-cor(my_data)
+corrplot(M2, p.mat = res1$p, sig.level = .05, type = "lower")
+
