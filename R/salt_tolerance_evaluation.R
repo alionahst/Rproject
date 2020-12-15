d50d97ae90f8f5ca19c73b923b108d8199423492
alionahst <aliona.96h@gmail.com>
2020-12-15 10:35:05 +0100

    15/12/20 10:34

--- R/salt_tolerance_evaluation.R
+++ R/salt_tolerance_evaluation.R
@@ -96,6 +96,8 @@ MFV <- MFV %>%
   mutate(Mean = rowMeans(MFV[1:5], na.rm=T))%>% #mutate(Mean = rowMeans(MFV[*column needed*], na.rm=T))%>%
   select(SL, RL, PH, NL, LA, FW, DW, RWC, EL, CC, Na, K, Ca, Mg, K_Na, PR, ICO2, TR, SC, Mean)
 
+# dendograme 
+
 
 # Pearson test
 cor.test(MFV$SL,MFV$RL, method="pearson") #
