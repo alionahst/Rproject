ac7d8957416e402040f8cf2d5109d5eb3f9a7724
Anastasiya Revva <anastasia.revva@gmail.comgit config --global user.email anastasia.revv@gmail.comgit config --global user.name anastasiia-revva>
2020-12-15 10:27:31 +0100

    201215 10:27

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -78,35 +78,95 @@ kruskal.test(Dry_Weight ~ Treat_Contr, data = table)
 
 
 #Mean and SD for variables
+x <- data.frame(dplyr::filter(table, table$Treat_Contr=="Treatment"))
+x <- x[-(1:3)]
+y <- data.frame(dplyr::filter(table, table$Treat_Contr=="Control"))
+y <- y[-(1:3)]
+
 Mean_Treat <- x %>% 
   summarise(Shoot_Length = mean(Shoot_Length),
             Root_Length = mean(Root_Length), 
             Plant_Height = mean(Plant_Height), 
             Number_Leaves = mean(Number_Leaves), 
-            Leaf_Area = mean(Leaf_Area)) 
+            Leaf_Area = mean(Leaf_Area),
+            Fresh_Weight = mean(Fresh_Weight),
+            Dry_Weight = mean(Dry_Weight),
+            RWC = mean(RWC),
+            Na = mean(Na),
+            K = mean(K),
+            Ca = mean(Ca),
+            Mg = mean(Mg),
+            K_NA = mean(K_Na),
+            Electrolyte_Leakage = mean(Electrolyte_Leakage),
+            Chlorophyll_Content = mean(Chlorophyll_Content),
+            Photsynthesis_Rate = mean (Photsynthesis_Rate, na.rm=T),
+            Intercellular_CO2 = mean(Intercellular_CO2, na.rm=T),
+            Transpiration_Rate = mean(Transpiration_Rate, na.rm=T),
+            Stomatal_Conductance = mean(Stomatal_Conductance, na.rm=T)) 
 Mean_Control <- y %>% 
   summarise(Shoot_Length = mean(Shoot_Length),
             Root_Length = mean(Root_Length), 
             Plant_Height = mean(Plant_Height), 
             Number_Leaves = mean(Number_Leaves), 
-            Leaf_Area = mean(Leaf_Area)) 
+            Leaf_Area = mean(Leaf_Area),
+            Fresh_Weight = mean(Fresh_Weight),
+            Dry_Weight = mean(Dry_Weight),
+            RWC = mean(RWC),
+            Na = mean(Na),
+            K = mean(K),
+            Ca = mean(Ca),
+            Mg = mean(Mg),
+            K_NA = mean(K_Na),
+            Electrolyte_Leakage = mean(Electrolyte_Leakage),
+            Chlorophyll_Content = mean(Chlorophyll_Content),
+            Photsynthesis_Rate = mean (Photsynthesis_Rate, na.rm=T),
+            Intercellular_CO2 = mean(Intercellular_CO2, na.rm=T),
+            Transpiration_Rate = mean(Transpiration_Rate, na.rm=T),
+            Stomatal_Conductance = mean(Stomatal_Conductance, na.rm=T)) 
 Treatment <- c('Treatment','Control')
-Means <- rbind(Mean_Treat,Mean_Control)
+Means <- rbind(Treatment, Mean_Treat)
 Means$Treat_Contr <- Treatment
 
 Sd_Treatment <- x %>%
-  select(1:5)%>%
-  summarise(SD_SL = sd(Shoot_Length),
+    summarise(SD_SL = sd(Shoot_Length),
             SD_RL = sd(Root_Length),
             SD_PH = sd(Plant_Height),
             SD_NL = sd(Number_Leaves),
-            SD_LA = sd(Leaf_Area))
+            SD_LA = sd(Leaf_Area),
+            SD_FW = sd(Fresh_Weight),
+            SD_DW = sd(Dry_Weight),
+            SD_RWC = sd(RWC),
+            SD_Na = sd(Na),
+            SD_K = sd(K),
+            SD_Ca = sd(Ca),
+            SD_Mg = sd(Mg),
+            SD_K_NA = sd(K_Na),
+            SD_EL = sd(Electrolyte_Leakage),
+            SD_CC = sd(Chlorophyll_Content),
+            SD_PR = sd (Photsynthesis_Rate, na.rm=T),
+            SD_I_CO2 = sd(Intercellular_CO2, na.rm=T),
+            SD_TR = sd(Transpiration_Rate, na.rm=T),
+            SD_SC = sd(Stomatal_Conductance, na.rm=T))
 Sd_Control <-   y %>%
   summarise(SD_SL = sd(Shoot_Length),
             SD_RL = sd(Root_Length),
             SD_PH = sd(Plant_Height),
             SD_NL = sd(Number_Leaves),
-            SD_LA = sd(Leaf_Area))
+            SD_LA = sd(Leaf_Area),
+            SD_FW = sd(Fresh_Weight),
+            SD_DW = sd(Dry_Weight),
+            SD_RWC = sd(RWC),
+            SD_Na = sd(Na),
+            SD_K = sd(K),
+            SD_Ca = sd(Ca),
+            SD_Mg = sd(Mg),
+            SD_K_NA = sd(K_Na),
+            SD_EL = sd(Electrolyte_Leakage),
+            SD_CC = sd(Chlorophyll_Content),
+            SD_PR = sd (Photsynthesis_Rate, na.rm=T),
+            SD_I_CO2 = sd(Intercellular_CO2, na.rm=T),
+            SD_TR = sd(Transpiration_Rate, na.rm=T),
+            SD_SC = sd(Stomatal_Conductance, na.rm=T))
 Treatment <- c('Treatment','Control')
 SD <- rbind(Sd_Treatment,Sd_Control)
 SD$Treat_Contr <- Treatment
