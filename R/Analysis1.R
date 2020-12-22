2aecec27ff50e3ce122f60a058d7b30b634072e6
Juan Felipe Villamil <juanfevil@wifialu32-64.wi-fi.upv.es>
2020-12-22 12:59:54 +0100

    22/12/2020

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -1,59 +1,13 @@
-#want to make a loop
-Normality <- data.frame()
-a1 <- numeric()
-for (i in names(table)[-(1:3)]){
-  a1 <- aov(i ~ Treat_Contr, data = table)
-  x <- shapiro.test(a1$residuals) 
-  Normality <- c(Normality, x)
-}
+#MORPHOLOGICAL TRAITS SHAPIRO AND KRUSKAL 
 
-
-boxplot(Shoot_Length ~ Treat_Contr, data = Morpho_t)
-boxplot(Root_Length ~ Treat_Contr, data = Morpho_t)
-boxplot(Height_Height ~ Treat_Contr, data = Morpho_t)
-boxplot(Number_Leaves ~ Treat_Contr, data = Morpho_t)
-boxplot(Leaf_Area ~ Treat_Contr, data = Morpho_t)
-
-
-a1 <- aov(Shoot_Length ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a1$residuals)
-#No normality
-kruskal.test(Shoot_Length ~ Treat_Contr, data = Morpho_t)
-# statistical difference
-
-a2 <- aov(Root_Length ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a2$residuals)
-#No normality
-kruskal.test(Root_Length ~ Treat_Contr, data = Morpho_t)
-# statistical difference
-
-
-a3 <- aov(Plant_Height ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a3$residuals)
-# No normality
-kruskal.test(Plant_Height ~ Treat_Contr, data = Morpho_t)
-# statistical difference
-
-
-a4 <- aov(Number_Leaves ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a4$residuals)
-# No normality 
-kruskal.test(Number_Leaves ~ Treat_Contr, data = Morpho_t)
-# statistical difference
-
-
-a5 <- aov(Leaf_Area ~ Treat_Contr, data = Morpho_t)
-shapiro.test(a5$residuals)
-# no normality 
-kruskal.test(Leaf_Area ~ Treat_Contr, data = Morpho_t)
-# statistical difference
-
-
-spm <- list()
+spm <-  numeric()
+nor_spm <- c()
 for(i in names(Morpho_t[4:8])) { 
   an1 <- aov(Morpho_t[, i] ~ Morpho_t$Treat_Contr)
   spm[i] <- shapiro.test(an1$residuals)$p.value
+  nor_spm <- c(nor_spm, ifelse (spm[i] <= 0.05, "False", "True"))
 }
+spm_table <- data.frame ("P-value" = spm, "Normality" = nor_spm)
 
 for (i in names(Morpho_t[4:8])) { 
   boxplot(Morpho_t[, i] ~ Morpho_t$Treat_Contr, 
@@ -63,27 +17,17 @@ for (i in names(Morpho_t[4:8])) {
   print(kruskal.test(Morpho_t[, i] ~ Morpho_t$Treat_Contr))
 }
 
-
-
-
 #Biomasse 
-a6 <- aov(Fresh_Weight ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a6$residuals)
-# no normality 
-kruskal.test(Fresh_Weight ~ Treat_Contr, data = Weight_ion)
-# Statistical difference 
 
-a7 <- aov(Dry_Weight ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a7$residuals)
-# no normality 
-kruskal.test(Dry_Weight ~ Treat_Contr, data = Weight_ion)
-# statistical difference
-
-spb <- list()
+spb <- numeric()
+nor_spb <- c()
 for(i in names(Weight_ion[4:5])) { 
   an2 <- aov(Weight_ion[, i] ~ Weight_ion$Treat_Contr)
   spb[i] <- shapiro.test(an2$residuals)$p.value
+  nor_spb <- c(nor_spb, ifelse (spb[i] <= 0.05, "False", "True"))
 }
+spb_table <- data.frame ("P-value" = spb, "Normality" = nor_spb)
+
 
 for (i in names(Weight_ion[4:5])) { 
   boxplot(Weight_ion[, i] ~ Weight_ion$Treat_Contr, 
@@ -93,33 +37,16 @@ for (i in names(Weight_ion[4:5])) {
   print(kruskal.test(Weight_ion[, i] ~ Weight_ion$Treat_Contr))
 }
 
-
-
 ###  RWC, EL and chlorophyl content
 
-a8 <- aov(RWC ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a8$residuals)
-# No normality
-kruskal.test(RWC ~ Treat_Contr, data = Weight_ion)
-# No statistical difference
-
-a9 <- aov(Electrolyte_Leakage ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a9$residuals)
-# No normality
-kruskal.test(Electrolyte_Leakage ~ Treat_Contr, data = Weight_ion)
-# Statistical difference
-
-a10 <- aov( Chlorophyll_Content ~ Treat_Contr, data = Chloro_c)
-shapiro.test(a10$residuals)
-# No normality
-kruskal.test(Chlorophyll_Content ~ Treat_Contr, data = Chloro_c)
-# Statistical difference
-
-spe <- list()
+spe <- numeric()
+nor_spe <- c()
 for(i in names(Weight_ion[c(6,12)])) { 
   an3 <- aov(Weight_ion[, i] ~ Weight_ion$Treat_Contr)
   spe[i] <- shapiro.test(an3$residuals)$p.value
+  nor_spe <- c(nor_spe, ifelse (spe[i] <= 0.05, "False", "True"))
 }
+spe_table <- data.frame ("P-value" = spe, "Normality" = nor_spe)
 
 
 for (i in names(Weight_ion[c(6,12)])) { 
@@ -130,11 +57,14 @@ for (i in names(Weight_ion[c(6,12)])) {
   print(kruskal.test(Weight_ion[, i] ~ Weight_ion$Treat_Contr))
 }
 
-spc <- list()
+spc <- numeric()
+nor_spc <- c()
 for(i in names(Chloro_c[4])) { 
   an4 <- aov(Chloro_c[, i] ~ Chloro_c$Treat_Contr)
   spc[i] <- shapiro.test(an4$residuals)$p.value
+  nor_spc <- c(nor_spc, ifelse (spc[i] <= 0.05, "False", "True"))
 }
+spc_table <- data.frame ("P-value" = spc, "Normality" = nor_spc)
 
 for (i in names(Chloro_c[4])) { 
   boxplot(Chloro_c[, i] ~ Chloro_c$Treat_Contr, 
@@ -144,45 +74,16 @@ for (i in names(Chloro_c[4])) {
   print(kruskal.test(Chloro_c[, i] ~ Chloro_c$Treat_Contr))
 }
 
-
-
-
 #### Mineral ion content
-a11 <- aov(Na ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a11$residuals)
-#No normality
-kruskal.test(Na ~ Treat_Contr, data = Weight_ion)
-# statistical difference
 
-a12 <- aov(K ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a12$residuals)
-# No normality
-kruskal.test(K ~ Treat_Contr, data = Weight_ion)
-# statistical difference
-
-a13 <- aov(Ca ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a13$residuals)
-#No normality
-kruskal.test(Ca ~ Treat_Contr, data = Weight_ion)
-# statistical difference
-
-a14 <- aov(Mg ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a14$residuals)
-#No normality
-kruskal.test(Mg ~ Treat_Contr, data = Weight_ion)
-# statistical difference
-
-a15 <- aov(K_Na ~ Treat_Contr, data = Weight_ion)
-shapiro.test(a15$residuals)
-#No normality
-kruskal.test(K_Na ~ Treat_Contr, data = Weight_ion)
-# statistical difference
-
-spw <- list()
+spw <- numeric()
+nor_spw <- c()
 for(i in names(Weight_ion[7:11])) { 
   an5 <- aov(Weight_ion[, i] ~ Weight_ion$Treat_Contr)
   spw[i] <- shapiro.test(an5$residuals)$p.value
+  nor_spw <- c(nor_spw, ifelse (spw[i] <= 0.05, "False", "True"))
 }
+spw_table <- data.frame ("P-value" = spw, "Normality" = nor_spw)
 
 for (i in names(Weight_ion[7:11])) { 
   boxplot(Weight_ion[, i] ~ Weight_ion$Treat_Contr, 
@@ -193,11 +94,16 @@ for (i in names(Weight_ion[7:11])) {
 }
 
 ## Gas parameters
-a16 <- aov(Photsynthesis_Rate ~ Treat_Contr, data = Gas_e)
-shapiro.test(a16$residuals)
-#No normality
-kruskal.test(Photsynthesis_Rate ~ Treat_Contr, data = Gas_e)
-# statistical difference
+
+spg <- numeric()
+nor_spg <- c()
+for (i in names(Gas_e[c(4,5,6:7)])) { 
+  an6 <- aov(Gas_e[, i] ~ Gas_e$Treat_Contr)
+  spg[i] <- shapiro.test(an6$residuals)$p.value
+  nor_spg <- c(nor_spg, ifelse (spg[i] <= 0.05, "False", "True"))
+}
+
+spg_table <- data.frame ("P-value" = spg, "Normality" = nor_spg)
 
 a17 <- aov(Intercellular_CO2 ~ Treat_Contr, data = Gas_e)
 shapiro.test(a17$residuals)
@@ -208,23 +114,6 @@ a17_aov
 #kruskal.test(Intercellular_CO2 ~ Treat_Contr, data = Gas_e)
 # No statistical difference
 
-a18 <- aov(Transpiration_Rate ~ Treat_Contr, data = Gas_e)
-shapiro.test(a18$residuals)
-#No normality
-kruskal.test(Transpiration_Rate ~ Treat_Contr, data = Gas_e)
-# statistical difference
-
-a19 <- aov(Stomatal_Conductance ~ Treat_Contr, data = Gas_e)
-shapiro.test(a19$residuals)
-#No normality
-kruskal.test(Stomatal_Conductance ~ Treat_Contr, data = Gas_e)
-# statistical difference
-
-spg <- list()
-for(i in names(Gas_e[c(4,6:7)])) { 
-  an6 <- aov(Gas_e[, i] ~ Gas_e$Treat_Contr)
-  spg[i] <- shapiro.test(an6$residuals)$p.value
-}
 
 for (i in names(Gas_e[4:7])) { 
   boxplot(Gas_e[, i] ~ Gas_e$Treat_Contr, 
