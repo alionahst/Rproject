206bdbb58f6a312ee1ca6e5bdb3e53b7e86b33d7
alionahst <aliona.96h@gmail.com>
2021-01-08 19:42:43 +0100

    08/01/21 19:42

--- R/Analysis1.R
+++ R/Analysis1.R
@@ -1,7 +1,11 @@
-install.packages("ggpubr")
+#install.packages("ggpubr")
+#install.packages("dplyr")
 library(ggpubr)
-#MORPHOLOGICAL TRAITS SHAPIRO AND KRUSKAL 
+library(dplyr)
 
+#MORPHOLOGICAL TRAITS 
+
+# Shapiro test
 spm <-  numeric()
 nor_spm <- c()
 for(i in names(Morpho_t[4:8])) { 
@@ -11,14 +15,7 @@ for(i in names(Morpho_t[4:8])) {
 }
 spm_table <- data.frame ("P-value" = spm, "Normality" = nor_spm)
 
-#for (i in names(Morpho_t[4:8])) { 
- # boxplot(Morpho_t[, i] ~ Morpho_t$Treat_Contr, 
-  #        ylab = names(Morpho_t[i]), 
-   #       xlab = names(Morpho_t[3])
-#  )
- # print(kruskal.test(Morpho_t[, i] ~ Morpho_t$Treat_Contr))
-#}
-
+# boxplot and statistical differences
 for (i in names(Morpho_t[4:8])) {
   bmorpho <- ggboxplot(Morpho_t, 
                  x = "Treat_Contr",
@@ -34,6 +31,7 @@ for (i in names(Morpho_t[4:8])) {
 
 #Biomasse 
 
+# Shapiro test
 spb <- numeric()
 nor_spb <- c()
 for(i in names(Weight_ion[4:5])) { 
@@ -43,14 +41,7 @@ for(i in names(Weight_ion[4:5])) {
 }
 spb_table <- data.frame ("P-value" = spb, "Normality" = nor_spb)
 
-
-#for (i in names(Weight_ion[4:5])) { 
- # boxplot(Weight_ion[, i] ~ Weight_ion$Treat_Contr, 
- #         ylab = names(Weight_ion[i]), 
-  #        xlab = names(Weight_ion[3])
- # )
- # print(kruskal.test(Weight_ion[, i] ~ Weight_ion$Treat_Contr))
-#}
+# boxplot and statistical differences
 for (i in names(Weight_ion[4:5])) {
   bwei <- ggboxplot(Weight_ion, 
                  x = "Treat_Contr",
@@ -65,7 +56,7 @@ for (i in names(Weight_ion[4:5])) {
 
 
 ###  RWC, EL and chlorophyl content
-
+# Shapiro test
 spe <- numeric()
 nor_spe <- c()
 for(i in names(Weight_ion[c(6,12)])) { 
@@ -75,15 +66,18 @@ for(i in names(Weight_ion[c(6,12)])) {
 }
 spe_table <- data.frame ("P-value" = spe, "Normality" = nor_spe)
 
+nor_spc <- c()
+an4 <- aov(Chloro_c$Chlorophyll_Content ~ Chloro_c$Treat_Contr)
+spc <- shapiro.test(an4$residuals)$p.value
+nor_spc <- c(nor_spc, ifelse (spc <= 0.05, "False", "True"))
 
-#for (i in names(Weight_ion[c(6,12)])) { 
- # boxplot(Weight_ion[, i] ~ Weight_ion$Treat_Contr, 
-  #        ylab = names(Weight_ion[i]), 
-   #       xlab = names(Weight_ion[3])
-#  )
- # print(kruskal.test(Weight_ion[, i] ~ Weight_ion$Treat_Contr))
-#}
+spc_table <- data.frame ("P-value" = spc, "Normality" = nor_spc)
+row.names(spc_table) <- "Chlorophyll_Content"
 
+spec_table <- dplyr::bind_rows(spe_table, spc_table)
+
+
+# boxplot and statistical differences
 for (i in names(Weight_ion[c(6,12)])) {
   bwei2 <- ggboxplot(Weight_ion, 
                  x = "Treat_Contr",
@@ -96,38 +90,18 @@ for (i in names(Weight_ion[c(6,12)])) {
   print(bwei2)
 } 
 
-spc <- numeric()
-nor_spc <- c()
-for(i in names(Chloro_c[4])) { 
-  an4 <- aov(Chloro_c[, i] ~ Chloro_c$Treat_Contr)
-  spc[i] <- shapiro.test(an4$residuals)$p.value
-  nor_spc <- c(nor_spc, ifelse (spc[i] <= 0.05, "False", "True"))
-}
-spc_table <- data.frame ("P-value" = spc, "Normality" = nor_spc)
-
-#for (i in names(Chloro_c[4])) { 
- # boxplot(Chloro_c[, i] ~ Chloro_c$Treat_Contr, 
-  #        ylab = names(Chloro_c[i]), 
-   #       xlab = names(Chloro_c[3])
-  #)
-  #print(kruskal.test(Chloro_c[, i] ~ Chloro_c$Treat_Contr))
-#}
-
-for (i in names(Chloro_c[4])) {
-  bchlo <- ggboxplot(Chloro_c, 
+bchlo <- ggboxplot(Chloro_c, 
                  x = "Treat_Contr",
-                 y = names(Chloro_c[i]), 
+                 y = "Chlorophyll_Content", 
                  color = names(Chloro_c[3]),
                  palette = c("blue", "red"),
-                 add = "jitter") + 
-    stat_compare_means(data = Chloro_c, method = "kruskal.test", label.x.npc = "center",
+                 add = "jitter") 
+bchlo + stat_compare_means(data = Chloro_c, method = "kruskal.test", label.x.npc = "center",
                        label.y.npc = "top")
-  print(bchlo)
-} 
 
 
 #### Mineral ion content
-
+# Shapiro test
 spw <- numeric()
 nor_spw <- c()
 for(i in names(Weight_ion[7:11])) { 
@@ -137,14 +111,7 @@ for(i in names(Weight_ion[7:11])) {
 }
 spw_table <- data.frame ("P-value" = spw, "Normality" = nor_spw)
 
-#for (i in names(Weight_ion[7:11])) { 
-#  boxplot(Weight_ion[, i] ~ Weight_ion$Treat_Contr, 
-#          ylab = names(Weight_ion[i]), 
- #         xlab = names(Weight_ion[3])
- # )
-  #print(kruskal.test(Weight_ion[, i] ~ Weight_ion$Treat_Contr))
-#}
-
+# boxplot and statistical differences
 for (i in names(Weight_ion[7:11])) {
   bwei3 <- ggboxplot(Weight_ion, 
                  x = "Treat_Contr",
@@ -158,7 +125,7 @@ for (i in names(Weight_ion[7:11])) {
 } 
 
 ## Gas parameters
-
+# Shapiro test
 spg <- numeric()
 nor_spg <- c()
 for (i in names(Gas_e[c(4,5,6:7)])) { 
@@ -166,45 +133,31 @@ for (i in names(Gas_e[c(4,5,6:7)])) {
   spg[i] <- shapiro.test(an6$residuals)$p.value
   nor_spg <- c(nor_spg, ifelse (spg[i] <= 0.05, "False", "True"))
 }
-
 spg_table <- data.frame ("P-value" = spg, "Normality" = nor_spg)
+
 
 a17 <- aov(Intercellular_CO2 ~ Treat_Contr, data = Gas_e)
 shapiro.test(a17$residuals)
-# NORMALITYYYYY
-#ANOVAAAA
-summary(a17)
-a17_aov <- agricolae::HSD.test(a17, "Treat_Contr")
 
-
-banova <- ggboxplot(Gas_e,
+# boxplot and statistical differences
+banova <- ggboxplot(Gas_e_withoutNA,
           x="Treat_Contr", 
           y= "Intercellular_CO2",
-          color = names(Gas_e[3]),
+          color = names(Gas_e_withoutNA[3]),
           palette = c("blue", "red"),
           add = "jitter") 
 banova  +   stat_compare_means(method = "anova")  
 
-#kruskal.test(Intercellular_CO2 ~ Treat_Contr, data = Gas_e)
-# No statistical difference
 
-
-#for (i in names(Gas_e[4:7])) { 
- # boxplot(Gas_e[, i] ~ Gas_e$Treat_Contr, 
-  #        ylab = names(Gas_e[i]), 
-  #        xlab = names(Gas_e[3])
-  #)
-  #print(kruskal.test(Gas_e[, i] ~ Gas_e$Treat_Contr))
-#}
-
-for (i in names(Gas_e[4,6:7])) {
-  bgas <- ggboxplot(Gas_e, 
+Gas_e_withoutNA <- na.omit(Gas_e)
+for (i in names(Gas_e_withoutNA[c(4,6:7)])) {
+  bgas <- ggboxplot(Gas_e_withoutNA, 
                  x = "Treat_Contr",
-                 y = names(Gas_e[i]), 
-                 color = names(Gas_e[3]),
+                 y = names(Gas_e_withoutNA[i]), 
+                 color = names(Gas_e_withoutNA[3]),
                  palette = c("blue", "red"),
                  add = "jitter") + 
-    stat_compare_means(data = Gas_e, method = "kruskal.test", label.x.npc = "center",
+    stat_compare_means(data = Gas_e_withoutNA, method = "kruskal.test", label.x.npc = "center",
                        label.y.npc = "top")
   print(bgas)
 } 
