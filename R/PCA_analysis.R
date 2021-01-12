38bcafbd4fbd6ede21a8277ba65711bbb1374b61
Anastasiya Revva <anastasia.revva@gmail.comgit config --global user.email anastasia.revv@gmail.comgit config --global user.name anastasiia-revva>
2021-01-12 12:54:36 +0100

    210112 12:54

--- R/PCA_analysis.R
+++ R/PCA_analysis.R
@@ -1,21 +1,68 @@
 ####------PCA on MFV-------###
+install.packages("factoextra")
+library(ggplot2)
+library(factoextra)
 
 #doing the PCA, adding the scaling into the function
-MFV_salt.pca <- prcomp((na.omit(MFV[-c(8, 17, 20)])), scale. = TRUE)
+MFV_salt.pca <- prcomp((na.omit(MFV[-c(8, 11:14, 17, 20)])), 
+                       scale. = TRUE)
 #results of the PCA
-MFV_salt.pca$sdev
-summary(MFV_salt.pca)
-#check, if the sums equal to quantity of observations (=17)
-sum((MFV_salt.pca$sdev)^2)
+#summary(MFV_salt.pca) - only needed for information to write r markdown
+
 #plotting the results in order to evaluate the principal components
-MFV_plot <- screeplot(MFV_salt.pca,
-                      npcs = length(MFV_salt.pca$sdev),
-                      type = "lines")
+fviz_eig(MFV_salt.pca,
+         title = "Variance contribution of principal components",
+         xlab = "Principal components")
+
 #plotting the results of the PCA
-library(ggplot2)
-#install.packages("ggfortify")
-library(ggfortify)
 #making a scatterplot of PC1 and PC2
+fviz_pca_ind(MFV_salt.pca,
+             repel = TRUE, # Avoid text overlapping
+             col.ind = "cos2",  # Individuals color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             #title = "Variance contribution of principal components",
+             #xlab = "Principal component 1 (34.4%)",
+             #ylab = "Principal component 2 (18.9%)",
+)
+fviz_pca_biplot(MFV_salt.pca,
+             repel = TRUE, # Avoid text overlapping
+             col.var = "contrib", # Variables color
+             col.ind = "black",  # Individuals color by the quality of representation
+             #gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             title = "Variance contribution of principal components",
+             xlab = "PCA 1 (34.4%)",
+             ylab = "PCA 2 (18.9%)"
+)
+fviz_pca_var(MFV_salt.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE,     # Avoid text overlapping
+             legend.title = "Groups"
+)
+
+fviz_pca_biplot(MFV_salt.pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+fviz_pca_ind(MFV_salt.pca,
+             col.ind = groups, # color by groups
+             palette = c("#00AFBB",  "#FC4E07"),
+            
+             legend.title = "Groups",
+             repel = TRUE
+)
+
+# Contributions of variables to PC1
+fviz_contrib(MFV_salt.pca, choice = "var", axes = 1, top = 13,
+             sortcontrib = "asc",
+             title = "Contribution of variables to PC1")
+# Contributions of variables to PC2
+fviz_contrib(MFV_salt.pca, choice = "var", axes = 2, top = 13,
+             title = "Contribution of variables to PC2")
+
+
+
 #creating data.frame for data labels in future plot
 MFV_labels <- data.frame(Acc_loc$Elevation[-c(7, 16)])
 row.names(MFV_labels) <- paste0("Ess-", c(1:6, 8:15, 17:25))
