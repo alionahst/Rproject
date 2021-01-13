1dc2fa76db6764487e2bfb8ffb346069c49e7303
Anastasiya Revva <anastasia.revva@gmail.comgit config --global user.email anastasia.revv@gmail.comgit config --global user.name anastasiia-revva>
2021-01-13 16:50:11 +0100

    210113 16:50

--- R/6.PCA_analysis.R
+++ R/6.PCA_analysis.R
@@ -12,28 +12,43 @@ MFV_salt.pca <- prcomp(MFV_salt,
 PCA_eval <- fviz_eig(MFV_salt.pca,
                      title = "Variance contribution of principal components",
                      xlab = "Principal components")
+print(PCA_eval)
 
 #creating vector for colors to correspond to ranking of accessions
-colors_PCA <- c(3:1, 3, 2, 2, 2, 3, 2, 3, 3, 3:1, 1, 1, 3, rep(2, 6))
+colors_PCA <- c("3. Tolerant", "2. Moderately tolerant", "1. Sensitive",
+                "3. Tolerant", "2. Moderately tolerant", "2. Moderately tolerant",
+                "2. Moderately tolerant", "3. Tolerant", "2. Moderately tolerant",
+                "3. Tolerant", "3. Tolerant", "3. Tolerant", "2. Moderately tolerant",
+                "1. Sensitive", "1. Sensitive", "1. Sensitive", "3. Tolerant",
+                "2. Moderately tolerant", "2. Moderately tolerant",
+                "2. Moderately tolerant", "2. Moderately tolerant",
+                "2. Moderately tolerant", "2. Moderately tolerant")
 
 #making a scatterplot of PC1 and PC2
 PCA_ind <- fviz_pca_ind(MFV_salt.pca,
                         repel = TRUE, # Avoid text overlapping
-                        geom = c("text", "point"),
                         labelsize = 5,
                         pointsize = 2,
                         habillage = colors_PCA,
                         addEllipses = TRUE,
                         ellipse.level = 0.95,
+                        mean.point = FALSE,
                         palette = c("#FF3333", "#009966", "#3366CC"),
-                        title = "PCA scatterplot")
+                        title = "PCA score plot")
+print(PCA_ind)
 
+#making loadings plot
 PCA_var <- fviz_pca_var(MFV_salt.pca,
                         col.var = "#2E9FDF",
                         repel = TRUE,
                         title = "PCA loadings")
+print(PCA_var)
 
 #Contributions of variables to PC1
-PCA_contr <- fviz_contrib(MFV_salt.pca, choice = "var", axes = 1, top = 13,
+PCA_contr <- fviz_contrib(MFV_salt.pca,
+                          choice = "var",
+                          axes = 1,
+                          top = 13,
                           sortcontrib = "asc",
                           title = "Contribution of variables to PC1")
+print(PCA_contr)
