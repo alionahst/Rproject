616b4f813013e5b82c89aedca3dca53596088fbb
Anastasiya Revva <anastasia.revva@gmail.comgit config --global user.email anastasia.revv@gmail.comgit config --global user.name anastasiia-revva>
2021-01-13 15:08:26 +0100

    210113 15:08

--- R/4.salt_tolerance_index.R
+++ R/4.salt_tolerance_index.R
@@ -2,49 +2,51 @@ x <- data.frame(dplyr::filter(table, table$Treat_Contr=="Treatment"))
 x <- x[-(1:3)]
 y <- data.frame(dplyr::filter(table, table$Treat_Contr=="Control"))
 y <- y[-(1:3)]
-
 #salt tolerance index for morphological traits
-STI_Shoot_Length <- x[1]/y[1]
-STI_Root_Length <- x[2]/y[2]
-STI_Plant_Height <- x[3]/y[3]
-STI_Leaf_Number <- x[4]/y[4]
-STI_Leaf_Area <- x[5]/y[5]
+STI_SL <- x[1]/y[1]
+STI_RL <- x[2]/y[2]
+STI_PH <- x[3]/y[3]
+STI_LN <- x[4]/y[4]
+STI_LA <- x[5]/y[5]
 #salt tolerance index for biomass
-STI_Fresh_Weight <- x[6]/y[6]
-STI_Dry_Weight <- x[7]/y[7]
+STI_FW <- x[6]/y[6]
+STI_DW <- x[7]/y[7]
 #salt tolerance index for RWC, EL and Chlorophyll content
-STI_Relative_water_content  <- x[8]/y[8]
-names(STI_Relative_water_content)[1] <- "Relative_water_content" # change the name of the column of the data
-STI_Electrolyte_Leakage  <- x[14]/y[14]
-STI_Chlorophyll_Content <- x[15]/y[15]
-# salt tolerance index for Mineral ion content
+STI_RWC  <- x[8]/y[8]
+STI_EL  <- x[14]/y[14]
+STI_CC <- x[15]/y[15]
+#salt tolerance index for Mineral ion content
 STI_Na <- x[9]/y[9]
 STI_K <- x[10]/y[10]
 STI_Ca <- x[11]/y[11]
 STI_Mg <- x[12]/y[12]
 STI_K_Na <- x[13]/y[13]
-# salt tolerance index for Gas exchange attributes
-STI_Photosynthesis_rate <- x[16]/y[16]
-STI_Intercellular_CO2 <- x[17]/y[17]
-STI_Transpiration_Rate <- x[18]/y[18]
-STI_Stomatal_Conductance <- x[19]/y[19]
+# salt tolerance index for gas exchange attributes
+STI_PR <- x[16]/y[16]
+STI_ICO2 <- x[17]/y[17]
+STI_TR <- x[18]/y[18]
+STI_SC <- x[19]/y[19]
 #Creating a summary data.frame
-STI <- data.frame(STI_Shoot_Length,
-                  STI_Root_Length, 
-                  STI_Plant_Height, 
-                  STI_Leaf_Number, 
-                  STI_Leaf_Area, 
-                  STI_Fresh_Weight, 
-                  STI_Dry_Weight,
-                  STI_Relative_water_content, 
-                  STI_Electrolyte_Leakage,
+STI <- data.frame(STI_SL,
+                  STI_RL, 
+                  STI_PH, 
+                  STI_LN, 
+                  STI_LA, 
+                  STI_FW, 
+                  STI_DW,
+                  STI_RWC, 
+                  STI_EL,
                   STI_Na,
                   STI_K,
                   STI_Ca,
                   STI_Mg,
                   STI_K_Na,
-                  STI_Chlorophyll_Content,
-                  STI_Photosynthesis_rate,
-                  STI_Intercellular_CO2,
-                  STI_Transpiration_Rate,
-                  STI_Stomatal_Conductance)
+                  STI_CC,
+                  STI_PR,
+                  STI_ICO2,
+                  STI_TR,
+                  STI_SC)
+#setting names of columns to compact the table view
+STI_names <- c("STI_SL", "STI_RL", "STI_PH", "STI_LN", "STI_LA", "STI_FW", "STI_DW", "STI_RWC", "STI_EL", "STI_Na", "STI_K",
+               "STI_Ca", "STI_Mg", "STI_K_Na", "STI_CC", "STI_PR", "STI_ICO2", "STI_TR", "STI_SC")
+names(STI) <- STI_names
--- R/5.membership_function_value.R
+++ R/5.membership_function_value.R
@@ -1,26 +1,24 @@
-# 1-results for the traits inversely related to salt tolerance: which are electrolyte leakage, Na, K and Ca
-# we know when to use the second equation based on pearson correlation.
-# 1-results for the traits inversely related to salt tolerance: which are electrolyte leakage, Na, K and Ca
+#Membership function value 
 library(dplyr)
+# 1-results for the traits inversely related to salt tolerance: which are electrolyte leakage, Na, K and Ca
 MFV <- STI %>%
-  mutate(SL = (STI$Shoot_Length - min(STI$Shoot_Length))/(max(STI$Shoot_Length)- min(STI$Shoot_Length)),
-         RL = (STI$Root_Length - min(STI$Root_Length))/(max(STI$Root_Length)- min(STI$Root_Length)),
-         PH = (STI$Plant_Height - min(STI$Plant_Height))/(max(STI$Plant_Height)- min(STI$Plant_Height)),
-         LN = (STI$Leaf_Number - min(STI$Leaf_Number))/(max(STI$Leaf_Number)- min(STI$Leaf_Number)),
-         LA = (STI$Leaf_Area - min(STI$Leaf_Area))/(max(STI$Leaf_Area)- min(STI$Leaf_Area)),
-         FW = (STI$Fresh_Weight - min(STI$Fresh_Weight))/(max(STI$Fresh_Weight)- min(STI$Fresh_Weight)) ,
-         DW = (STI$Dry_Weight - min(STI$Dry_Weight))/(max(STI$Dry_Weight)- min(STI$Dry_Weight)),
-         RWC = (STI$Relative_water_content - min(STI$Relative_water_content))/(max(STI$Relative_water_content)- min(STI$Relative_water_content)),
-         EL = 1-(STI$Electrolyte_Leakage - min(STI$Electrolyte_Leakage))/(max(STI$Electrolyte_Leakage)- min(STI$Electrolyte_Leakage)),
-         CC = (STI$Chlorophyll_Content - min(STI$Chlorophyll_Content))/(max(STI$Chlorophyll_Content)- min(STI$Chlorophyll_Content)),
-         Na = 1-(STI$Na - min(STI$Na))/(max(STI$Na)- min(STI$Na)),
-         K = 1-(STI$K - min(STI$K))/(max(STI$K)- min(STI$K)),
-         Ca =1- (STI$Ca - min(STI$Ca))/(max(STI$Ca)- min(STI$Ca)),
-         Mg=(STI$Mg - min(STI$Mg))/(max(STI$Mg)- min(STI$Mg)),
-         K_Na= (STI$K_Na - min(STI$K_Na))/(max(STI$K_Na)- min(STI$K_Na)),
-         PR= (STI$Photsynthesis_Rate - min(STI$Photsynthesis_Rate, na.rm=T))/(max(STI$Photsynthesis_Rate, na.rm=T)- min(STI$Photsynthesis_Rate, na.rm=T)),
-         ICO2= (STI$Intercellular_CO2 - min(STI$Intercellular_CO2, na.rm=T))/(max(STI$Intercellular_CO2, na.rm=T)- min(STI$Intercellular_CO2, na.rm=T)),
-         TR= (STI$Transpiration_Rate - min(STI$Transpiration_Rate, na.rm=T))/(max(STI$Transpiration_Rate, na.rm=T)- min(STI$Transpiration_Rate, na.rm=T)),
-         SC= (STI$Stomatal_Conductance - min(STI$Stomatal_Conductance, na.rm=T))/(max(STI$Stomatal_Conductance, na.rm=T)- min(STI$Stomatal_Conductance, na.rm=T)))%>%
+  mutate(SL = (STI$STI_SL - min(STI$STI_SL))/(max(STI$STI_SL)- min(STI$STI_SL)),
+         RL = (STI$STI_RL - min(STI$STI_RL))/(max(STI$STI_RL)- min(STI$STI_RL)),
+         PH = (STI$STI_PH - min(STI$STI_PH))/(max(STI$STI_PH)- min(STI$STI_PH)),
+         LN = (STI$STI_LN - min(STI$STI_LN))/(max(STI$STI_LN)- min(STI$STI_LN)),
+         LA = (STI$STI_LA - min(STI$STI_LA))/(max(STI$STI_LA)- min(STI$STI_LA)),
+         FW = (STI$STI_FW - min(STI$STI_FW))/(max(STI$STI_FW)- min(STI$STI_FW)),
+         DW = (STI$STI_DW - min(STI$STI_DW))/(max(STI$STI_DW)- min(STI$STI_DW)),
+         RWC = (STI$STI_RWC - min(STI$STI_RWC))/(max(STI$STI_RWC)- min(STI$STI_RWC)),
+         EL = 1-(STI$STI_EL - min(STI$STI_EL))/(max(STI$STI_EL)- min(STI$STI_EL)),
+         CC = (STI$STI_CC - min(STI$STI_CC))/(max(STI$STI_CC)- min(STI$STI_CC)),
+         Na = 1-(STI$STI_Na - min(STI$STI_Na))/(max(STI$STI_Na)- min(STI$STI_Na)),
+         K = 1-(STI$STI_K - min(STI$STI_K))/(max(STI$STI_K)- min(STI$STI_K)),
+         Ca = 1- (STI$STI_Ca - min(STI$STI_Ca))/(max(STI$STI_Ca)- min(STI$STI_Ca)),
+         Mg = (STI$STI_Mg - min(STI$STI_Mg))/(max(STI$STI_Mg)- min(STI$STI_Mg)),
+         K_Na = (STI$STI_K_Na - min(STI$STI_K_Na))/(max(STI$STI_K_Na)- min(STI$STI_K_Na)),
+         PR = (STI$STI_PR - min(STI$STI_PR, na.rm=T))/(max(STI$STI_PR, na.rm=T)- min(STI$STI_PR, na.rm=T)),
+         ICO2 = (STI$STI_ICO2 - min(STI$STI_ICO2, na.rm=T))/(max(STI$STI_ICO2, na.rm=T)- min(STI$STI_ICO2, na.rm=T)),
+         TR = (STI$STI_TR - min(STI$STI_TR, na.rm=T))/(max(STI$STI_TR, na.rm=T)- min(STI$STI_TR, na.rm=T)),
+         SC = (STI$STI_SC - min(STI$STI_SC, na.rm=T))/(max(STI$STI_SC, na.rm=T)- min(STI$STI_SC, na.rm=T)))%>%
   select(SL, RL, PH, LN, LA, FW, DW, RWC, EL, CC, Na, K, Ca, Mg, K_Na, PR, ICO2, TR, SC)
-
